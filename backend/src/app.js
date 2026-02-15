b0fea17bbd65bb38b0193e3ac54031fc1ede1cb7
Omkar gavade <Omkargavade002@gmail.com>
2026-02-15 22:01:58 +0530

    Initial backend setup with Express, Socket.IO, MongoDB

---
+++ backend/src/app.js
@@ -0,0 +1,36 @@
+import express from "express";
+import {createServer} from "node:http";
+
+import { Server } from "socket.io";
+
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+import connectToSocket from "./controllers/socketManager.js";
+
+import cors from "cors";
+
+const app = express();
+const server = createServer(app);
+const io = connectToSocket(server);
+
+app.set("port", (process.env.PORT || 8000));
+app.use(cors());
+app.use(express.json({limit: "40kb"}));
+app.use(express.urlencoded({limit: "40kb", extended: true }));
+
+
+const start = async () => {
+    
+    const connectionDb = await mongoose.connect(process.env.MONGO_URI);
+
+    console.log(`MONGO Connected Host ${connectionDb.connection.host}`);
+
+    server.listen(app.get("port"),() =>{
+        console.log("LISTINING ON PORT 8000")
+    });
+
+}
+
+start();
